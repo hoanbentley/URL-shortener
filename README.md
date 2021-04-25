839cb4ef84b279f52923671d1d94081720d9dee0
hoantk <hoantk@sendo.vn>
2021-04-25 14:22:32 +0700

    [main] - write unit test - add comment - readme.rm

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# URL-shortener
+### Overview
+This is a service which named [URL-shortener] for a shorten link, this service base-on clean architecture, right now this service can handle login/list/create/search/redirect functions.
+To make it run:
+- `go run main.go`
+- Import Postman collection from `doc` to check example
+
+### List API
+- Login
+- Redirect
+- Create
+- List
+- Search
+- Delete
+
+![alt text](https://github.com/hoanbentley/URL-shortener/tree/main/doc/structure.png?raw=true)
+
+### DB Schema
+```sql
+-- users definition
+
+CREATE TABLE users (
+	user_id TEXT NOT NULL,
+	password TEXT NOT NULL
+);
+
+INSERT INTO users (user_id, password) VALUES('admin', 'admin');
+
+-- urls definition
+
+CREATE TABLE urls (
+	id INTEGER PRIMARY KEY,
+	short_code TEXT NOT NULL,
+	full_url TEXT NOT NULL,
+    created_date INTEGER,
+	expiry INTEGER,
+	number_of_hits INTEGER
+);
+```
--- main.go
+++ main.go
@@ -21,35 +21,4 @@ func main() {
 	router.HandleFunc("/create", todo.CreateUrl).Methods(http.MethodPost)
 	router.HandleFunc("/{id}", todo.RedirectUrl).Methods(http.MethodGet)
 	http.ListenAndServe(":8080", router)
-
-	// statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS urls (id INTEGER PRIMARY KEY, short_code TEXT, full_url TEXT, expiry INTEGER, create_date INTEGER, number_of_hits INTEGER)")
-	/*database, _ := sql.Open("sqlite3", "./rabbit.db")
-	statement, _ := database.Prepare("DELETE FROM urls;")
-	statement.Exec()*/
-
-	/*database, _ := sql.Open("sqlite3", "./rabbit.db")
-	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS users (user_id TEXT, password TEXT)")
-	statement.Exec()
-	statement, _ = database.Prepare("INSERT INTO users (user_id, password) VALUES (?, ?)")
-	statement.Exec("admin", "admin")
-	rows, _ := database.Query("SELECT user_id, password FROM users")
-	var user_id string
-	var password string
-	for rows.Next() {
-		rows.Scan(&user_id, &password)
-		fmt.Println(user_id + " " + password)
-	}*/
-
-	/*statement, _ = database.Prepare("INSERT INTO urls (short_code, full_url, expiry, number_of_hits) VALUES (?, ?, ?, ?)")
-	statement.Exec("Nic", "Raboy", "hoantk", "1")
-	rows, _ := database.Query("SELECT id, shortcode,fullurl, expiry,numberofhits  FROM urls")
-	var id int
-	var shortcode string
-	var fullurl string
-	var expiry string
-	var numberofhits string
-	for rows.Next() {
-		rows.Scan(&id, &shortcode, &fullurl, &expiry, &numberofhits)
-		fmt.Println(strconv.Itoa(id) + ": " + shortcode + " " + fullurl + " " + expiry + " " + numberofhits)
-	}*/
 }
