5bd7b5550c44c7e687360ac2a7645b3c4f8df04c
hoantk@sendo.vn <hoantk@sendo.vn>
2021-04-26 09:47:29 +0700

    [main] escape input params of search function

--- internal/usecase/search_url_uc.go
+++ internal/usecase/search_url_uc.go
@@ -3,8 +3,13 @@ package usecase
 import (
 	"context"
 	"github.com/hoanbentley/URL-shortener/internal/entities"
+	"html"
 )
 
 func (u *uc) SearchUrl(ctx context.Context, shortCode, fullUrl string) ([]*entities.Urls, error) {
-	return u.url.SearchUrl(ctx, shortCode, fullUrl)
+	//escape url string xss url
+	escapeShortCode := html.EscapeString(shortCode)
+	escapeFullUrl := html.EscapeString(fullUrl)
+
+	return u.url.SearchUrl(ctx, escapeShortCode, escapeFullUrl)
 }
